ec0b99b47ff01f06383bca0bb5cd9935b5e2813e
xikkbae <nickjiang356@gmail.com>
2023-03-02 09:20:27 +1300

    First Commit

--- Chess Board.py
+++ Chess Board.py
@@ -1,1 +1,18 @@
-print ("This is an update to see the history feature")
+# Dimensions of chess board
+dimensions = 8
+# Assigning each piece a letter
+knight = "N"
+rook = "R"
+bishop = "B"
+king = "K"
+Queen = "Q"
+Pawn = "P"
+
+# Printing the board
+board = ""
+def print_board(dimensions,board):
+    for i in range(dimensions):
+        board = board + "0 "
+    for i in range(dimensions):
+        print(board)
+print_board(dimensions,board)

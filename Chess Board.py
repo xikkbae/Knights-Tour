29fe207f416664c14ca9407e765f5b68ea995a40
xikkbae <nickjiang356@gmail.com>
2023-03-09 11:44:57 +1300

    This prints out a whole chess board with the correct pieces

--- Chess Board.py
+++ Chess Board.py
@@ -7,12 +7,21 @@ bishop = "B"
 king = "K"
 Queen = "Q"
 Pawn = "P"
-
+order = ["R", "B", "N", "Q", "K", "N", "B", "R"]
+order_string = "R B N Q K N B R"
 # Printing the board
 board = ""
+
 def print_board(dimensions,board):
+    print(order_string)
+    print("P P P P P P P P")
     for i in range(dimensions):
         board = board + "0 "
-    for i in range(dimensions):
+    for i in range(dimensions - 4):
         print(board)
+    print("P P P P P P P P")
+    print(order_string)
+
 print_board(dimensions,board)
+
+

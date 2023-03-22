81a03dfd09e9ba82636d66d5be965b783f42c43a
xikkbae <nickjiang356@gmail.com>
2023-03-23 12:30:30 +1300

    I realised I have to make a 2D array so I restarted on my project

--- Chess Board.py
+++ Chess Board.py
@@ -1,4 +1,4 @@
-# Dimensions of chess board
+'''# Dimensions of chess board
 dimensions = 8
 # Assigning each piece a letter
 knight = "N"
@@ -9,10 +9,73 @@ Queen = "Q"
 Pawn = "P"
 order = ["R", "B", "N", "Q", "K", "N", "B", "R"]
 order_string = "R B N Q K N B R"
-# Printing the board
-board = ""
+'''
 
-def print_board(dimensions,board):
+# A 2D array for the board
+board = [[0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0]]
+
+def print_board(): # Function for printing the board
+    for row in range(8):
+        for column in range(8):
+            print(board[row][column], end=" ")
+        print("\n")
+
+
+
+
+
+
+
+'''board = ""
+coord_x = 0
+coord_y = 0
+x_move = [2, 1, -1, -2, -2, -1, 1, 2]
+y_move = [1, 2, 2, 1, -1, -2, -2, -1]
+board = [[-1 for i in range(dimensions)]for i in range(dimensions)]
+board[0][0] = 0
+step=1
+'''
+
+
+
+
+
+
+
+
+'''def solveKTUtil(dimensions, board, curr_x, curr_y, x_move, y_move, step):
+    
+        A recursive utility function to solve Knight Tour
+        problem
+
+    if (step == dimensions ** 2):
+        return True
+
+    # Try all next moves from the current coordinate x, y
+    for i in range(8):
+        new_x = curr_x + move_x[i]
+        new_y = curr_y + move_y[i]
+        if (isSafe(new_x, new_y, board)):
+            board[new_x][new_y] = pos
+            if (solveKTUtil(n, board, new_x, new_y, move_x, move_y, pos + 1)):
+                return True
+
+            # Backtracking
+            board[new_x][new_y] = -1
+    return False
+'''
+
+
+
+# Printing the board
+'''def print_board(dimensions,board):
     print(order_string)
     print("P P P P P P P P")
     for i in range(dimensions):
@@ -21,8 +84,6 @@ def print_board(dimensions,board):
         print(board)
     print("P P P P P P P P")
     print(order_string)
-horizontal_move = [2, 1, -1, -2, -2, -1, 1, 2]
-vertical_move = [1, 2, 2, 1, -1, -2, -2, -1]
-print_board(dimensions,board)
+print_board(dimensions,board)'''
 
 
